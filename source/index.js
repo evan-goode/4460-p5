a91405d62af6563b40ddd4edad5efbf61a451b66
Evan Goode <mail@evangoo.de>
2019-04-19 15:25:59 -0400

    nice animations, mosaic plot working

--- source/index.js
+++ source/index.js
@@ -1,10 +1,12 @@
 import * as d3 from "d3";
 import * as Sortable from "sortablejs";
 import { readFileSync } from "fs";
+import _ from "lodash";
 
 const DATA = JSON.parse(readFileSync(`${__dirname}/data.json`, "utf-8"));
+const INITIAL_SELECTION = _.maxBy(DATA, d => d.ratings.joy);
 
-const ANIMATION_DURATION = 150;
+const ANIMATION_DURATION = 500; // milliseconds
 
 const BAR_HEIGHT = 20;
 const BAR_CHART_WIDTH = 640;
@@ -34,18 +36,7 @@ const MOSAIC_MARGIN = {
 	left: 0
 };
 
-const barChart = d3
-	.select("#bar-chart")
-	.append("svg")
-	.attr(
-		"viewBox",
-		`${-1 * BAR_MARGIN.left} ${-1 * BAR_MARGIN.top} ${BAR_MARGIN.left +
-			BAR_CHART_WIDTH +
-			BAR_MARGIN.right} ${BAR_MARGIN.top +
-			BAR_CHART_HEIGHT +
-			BAR_MARGIN.bottom}`
-	);
-
+// mosaic chart
 const mosaicChart = d3
 	.select("#mosaic-chart")
 	.append("svg")
@@ -58,34 +49,54 @@ const mosaicChart = d3
 			MOSAIC_MARGIN.bottom}`
 	);
 
-const hierarchy = d3.hierarchy(DATA[0]).sum(d => d.value);
-
-const root = d3
+const treemap = d3
 	.treemap()
 	.size([MOSAIC_CHART_WIDTH, MOSAIC_CHART_HEIGHT])
 	.paddingInner(10)
-	.tile(d3.treemapSliceDice)(hierarchy);
+	.tile(d3.treemapSliceDice);
 
-mosaicChart
+const generateMosaicChart = selection => {
+	const hierarchy = d3.hierarchy(selection).sum(d => d.value);
+	const root = treemap(hierarchy);
+	return root;
+};
+
+const root = generateMosaicChart(INITIAL_SELECTION);
+const boxElements = mosaicChart
 	.selectAll(".box")
 	.data(root.leaves())
 	.enter()
 	.append("rect")
-	.attr("x", d => d.x0)
-	.attr("y", d => d.y0)
-	.attr("width", d => d.x1 - d.x0)
-	.attr("height", d => d.y1 - d.y0)
-	.attr("stroke", "white")
+	.attr("class", "box")
 	.attr("fill", d => COLORS[d.data.name]);
 
-const barY = d3
-	.scaleLinear()
-	.range([0, BAR_CHART_WIDTH])
-	.domain([0, 1]);
+const updateMosaicChart = (selection, animate) => {
+	const root = generateMosaicChart(selection);
+	boxElements
+		.data(root.leaves())
+		.transition()
+		.duration(animate ? ANIMATION_DURATION : 0)
+		.attr("x", d => d.x0)
+		.attr("y", d => d.y0)
+		.attr("width", d => d.x1 - d.x0)
+		.attr("height", d => d.y1 - d.y0);
+};
+updateMosaicChart(INITIAL_SELECTION, false);
 
-const barAxisXElement = barChart.append("g");
+// bar chart
+const barChart = d3
+	.select("#bar-chart")
+	.append("svg")
+	.attr(
+		"viewBox",
+		`${-1 * BAR_MARGIN.left} ${-1 * BAR_MARGIN.top} ${BAR_MARGIN.left +
+			BAR_CHART_WIDTH +
+			BAR_MARGIN.right} ${BAR_MARGIN.top +
+			BAR_CHART_HEIGHT +
+			BAR_MARGIN.bottom}`
+	);
 
-const generateChart = order => {
+const generateBarChart = order => {
 	const sorted = DATA.sort((a, b) => {
 		return d3.descending(a.ratings[order[0]], b.ratings[order[0]]);
 	});
@@ -104,41 +115,55 @@ const generateChart = order => {
 	return { stacks, barX, barAxisX };
 };
 
-const { stacks, barX, barAxisX } = generateChart(RATINGS);
-const stackElements = barChart.selectAll(".stack")
-	.data(stacks)
-	.attr("class", "stack")
-	.enter().append("g")
-		.attr("fill", d => COLORS[d.key])
+const barY = d3
+	.scaleLinear()
+	.range([0, BAR_CHART_WIDTH])
+	.domain([0, 1]);
 
-const barElements = stackElements.selectAll(".bar")
+const barAxisXElement = barChart.append("g");
+
+const { stacks, barX, barAxisX } = generateBarChart(RATINGS);
+const stackElements = barChart
+	.selectAll(".stack")
+	.data(stacks)
+	.enter()
+	.append("g")
+	.attr("class", "stack")
+	.attr("fill", d => COLORS[d.key]);
+
+const barElements = stackElements
+	.selectAll(".bar")
 	.data(d => d)
 	.enter()
 	.append("rect")
 	.attr("class", "bar")
-	.attr("stroke", "black")
-	.attr("x", d => barY(d[0]))
-	.attr("y", d => barX(d.data.name))
-	.attr("width", d => barY(d[1]) - barY(d[0]))
-	.attr("height", d => barX.bandwidth())
+	.attr("stroke", "black");
 
-const update = (order, selected) => {
-	const { stacks, barX, barAxisX } = generateChart(order);
-	stackElements.data(stacks)
+const updateBarChart = (order, animate) => {
+	const { stacks, barX, barAxisX } = generateBarChart(order);
+	stackElements
+		.data(stacks)
+		.transition()
+		.duration(animate ? ANIMATION_DURATION : 0)
 		.attr("fill", d => COLORS[d.key]);
-	barElements.data(d => d)
+	barElements
+		.data(d => d)
+		.transition()
+		.duration(animate ? ANIMATION_DURATION : 0)
 		.attr("x", d => barY(d[0]))
 		.attr("y", d => barX(d.data.name))
 		.attr("width", d => barY(d[1]) - barY(d[0]))
-		.attr("height", d => barX.bandwidth())
-	barAxisXElement.call(barAxisX);
+		.attr("height", d => barX.bandwidth());
+	barAxisXElement
+		.transition()
+		.duration(animate ? ANIMATION_DURATION : 0)
+		.call(barAxisX);
 };
-update(RATINGS);
+updateBarChart(RATINGS, false);
 
-barChart.selectAll(".bar").on("click", d => {
-	
-});
+barChart.selectAll(".bar").on("click", d => updateMosaicChart(d.data, true));
 
+// reorder
 const reorderContainerElement = document.querySelector("#reorder-container");
 const totalBarChartWidth = BAR_MARGIN.left + BAR_CHART_WIDTH + BAR_MARGIN.right;
 reorderContainerElement.style.paddingLeft = `${(100 * BAR_MARGIN.left) /
@@ -162,5 +187,5 @@ RATINGS.map(rating => {
 const sortable = Sortable.create(reorderElement, {
 	animation: ANIMATION_DURATION,
 	dataIdAttr: "data-id",
-	onEnd: event => update(sortable.toArray())
+	onEnd: event => updateBarChart(sortable.toArray(), true)
 });
